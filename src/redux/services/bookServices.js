1084690016f7f1271163ba7a204b34f5e7385ad1
Batikann <emiruar123@gmail.com>
2023-04-17 23:40:45 +0300

    added comment line

--- src/redux/services/bookServices.js
+++ src/redux/services/bookServices.js
@@ -2,6 +2,7 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 const API_URL = 'https://www.googleapis.com/books/v1/volumes?q='
 
+//Axios kullanarak createAsyncThunk methodu ile kullanıcının isteğini gerçekleştirecek olan servis
 export const fetchBooks = createAsyncThunk('fetchBooks', async (query) => {
   const { data: res } = await axios.get(
     `${API_URL}${query}&maxResults=20&key=${import.meta.env.VITE_REACT_API_URL}`
